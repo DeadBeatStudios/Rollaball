9f553cbcbc157b454365f247a67f82596ee4a934
Deadbeats89 <Dodgen1989@Gmail.com>
2025-11-21 05:37:51 -0500

    FlagGoal
    Started Adding FlagGoal

--- Assets/Scripts/UI/Managers/GoalTrigger.cs
+++ Assets/Scripts/UI/Managers/GoalTrigger.cs
@@ -1,16 +1,35 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GoalTrigger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Scoring")]
+    [SerializeField] private int pointsPerScore = 100;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        // Only react to players
+        if (!other.CompareTag("Player"))
+            return;
+
+        // Does this player have PlayerScore?
+        PlayerScore scorer = other.GetComponent<PlayerScore>();
+        if (scorer == null)
+            return;
+
+        // Does the player currently hold the flag?
+        FlagPickup flag = FindObjectOfType<FlagPickup>();
+        if (flag == null)
+            return;
+
+        if (!flag.IsHeldBy(other.transform))
+            return;
+
+        // Award points
+        scorer.AddPoints(pointsPerScore);
+
+        // Reset the flag
+        flag.DropAndRespawn(FlagPickup.FlagDropCause.SelfDestruct);
+
+        Debug.Log($"🏁 Player {scorer.playerID} scored! +{pointsPerScore} points");
     }
 }
